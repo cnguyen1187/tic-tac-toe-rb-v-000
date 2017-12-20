e72cfcc8875548dfa84de865f7c9c62db86e60db
Cam Nguyen <cnguyen1187@gmail.com>
2017-12-20 22:24:56 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -20,3 +20,11 @@ end
 def input_to_index(user_input)
   user_input.to_i - 1
 end
+
+def move(board, index, player)
+  board[index] = player
+end
+
+def position_taken?(board, index)
+  board[index] != " "
+end
