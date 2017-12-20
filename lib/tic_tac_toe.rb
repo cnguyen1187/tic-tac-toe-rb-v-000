b37201be9aa72e73720fd448b7d0c1047d1042fa
Cam Nguyen <cnguyen1187@gmail.com>
2017-12-20 22:26:13 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -44,3 +44,22 @@ def turn(board)
     turn(board)
   end
 end
+
+def turn_count(board)
+  counter = 0
+  board.each do |move|
+    if move == "X" || move == "O"
+      counter += 1
+    end
+  end
+  return counter
+end
+
+def current_player(board)
+  turn_count(board)
+  if  turn_count(board) % 2 == 0
+    return "X"
+  else
+    return "O"
+  end
+end
